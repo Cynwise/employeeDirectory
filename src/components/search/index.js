e1bb7416ab43eb278ad4f6eb924723392470393b
William Farnsworth <williamcoreyf@gmail.com>
2021-06-22 14:55:11 -0400

    Changed wording around

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -10,7 +10,7 @@ const search = (props) => {
     return (
         <div style = {{margin: "3vw 24vw 3vw 24vw"}}>
           <form>
-            <input onChange = {handleChange} style = {{width: "50vw"}} placeholder="Search by name, phone, email, or location" name="search" type="search" className="form-control-lg search-font mx-auto"/>
+            <input onChange = {handleChange} style = {{width: "50vw"}} placeholder="Search..." name="search" type="search" className="form-control-lg search-font mx-auto"/>
           </form>
         </div>
       );
