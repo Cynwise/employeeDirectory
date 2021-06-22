3a69c135535a22d5582ae1e8bc6b836a9fb532c4
William Farnsworth <williamcoreyf@gmail.com>
2021-06-21 21:34:20 -0400

    created header page component

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+
+    return(
+
+        <div className="p-5 text-center bg-dark">
+
+            <h1 className = "mb-3 text-light">Employee Directory</h1>
+            <h4 className = "mb-3 text-light">Search the Employee Directory</h4>
+        </div>
+    )
+}
+
+export default Header
